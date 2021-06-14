1616a3e785a9045882825131700e8d8b5469b0a9
Lucas Wung <tkoppopwung130@gmail.com>
2021-06-14 16:59:21 -0700

    init

---
+++ main.py
@@ -0,0 +1,68 @@
+import discord
+import random
+from discord.ext import commands
+
+client = commands.Bot(command_prefix = '.')
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'Pong! {round(client.latency*1000)}ms')
+
+@client.command()
+async def clear(ctx, amount=5):
+    await ctx.channel.purge(limit=amount)
+
+@client.command()
+async def kick(ctx,member : discord.Member, *, reason=None):
+    await member.kick(reason = reason)
+
+@client.command()
+async def ban(ctx,member : discord.Member, *, reason=None):
+    await member.ban(reason=reason)
+
+@client.command()
+async def unban(ctx, *, member):
+    banned_users = await ctx.guild.bans()
+    member_name, member_discriminator = member.split('#')    
+
+    for ban_entry in banned_users:
+        user = ban_entry.user
+
+        if(user.name, user.discriminator) == (member_name, member_discriminator):
+            await ctx.guild.unban(user)
+            await ctx.send(f'Unbanned {user.mention}')
+            return
+
+            
+
+
+@client.command(aliases=['8ball'])
+async def _8ball(ctx, *, question):
+    responses = ['It is certain.',
+                'It is decidedly so.',
+                'Without a doubt',
+                'Yes - definitely.',
+                'You may rely on it.',
+                'As I see it, yes.',
+                'Most likely.',
+                'The outlook is good',
+                'Yes.',
+                'Signs point to yes.',
+                'Reply hazy, try again.',
+                'Ask again later.',
+                'Better not tell you now',
+                'Cannot predict now.', 
+                'Concentrate and ask again.',
+                "Don't count on it.",
+                'My reply is no',
+                'My sources say no.',
+                'Outlook not so good.',
+                'Very doubtful.']
+    await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
+
+
+client.run('ODU0MTI5MTgyNjQ3NTE3MTg1.YMfbyg.OL2mrQACSV83A1miVYty4p8u9xk')
---
+++ riotapi.py
@@ -0,0 +1,26 @@
+from riotwatcher import LolWatcher
+key = 'RGAPI-d2347ac6-9736-49f7-b191-1b081f1ba264'
+watcher = LolWatcher(key)
+
+
+def printStats(summonerName):
+    summoner = watcher.summoner.by_name('na1',summonerName)
+    stats = watcher.league.by_summoner('na1', summoner['id'])
+    num = 0
+    if (stats[0]['queueType'] == 'RANKED_SOLO_5x5'):
+        num = 0
+    else:
+        num = 1
+
+    tier = stats[num]['tier']
+    rank = stats[num]['rank']
+    lp = stats[num]['leaguePoints']
+
+    wins= int(stats[num]['wins'])
+    losses = int(stats[num]['losses'])
+    wr = int((wins/(wins+losses))* 100)
+    print(stats)
+    print(summonerName +" is currently ranked in " + str(tier), str(rank) +" with " + str(lp) + "LP and a " + str(wr) + "% winrate.")
+
+
+printStats("marcø")
